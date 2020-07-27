9b7a2ab871d4e57895f063c3060d57e8b4bc4ab6
lukabajic <lukabajic.dev@gmail.com>
2020-07-27 13:22:34 +0200

    refactor: Cleanup
     - Delted unnecesary lines/imports created by create-react-app in App.js and index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
+import "./style.css";
 import App from "./App";
 
 ReactDOM.render(
@@ -9,5 +9,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-serviceWorker.unregister();
