a9492faf32a0207b401e3ff6f1bad7d71b2e4249
lukabajic <lukabajic.dev@gmail.com>
2020-07-28 11:41:37 +0200

    feat: Added Footer component
     - style: Hr style added
     - Footer reusable component done (may add on it in the future)
     - style: Centered social media links on footer/nav

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,9 +1,22 @@
 import React from "react";
 import "./Footer.css";
 
+import SMLinks from "../../Utility/SMLinks/SMLinks";
+
 const Footer = ({ navOpen }) => {
   return (
-    <footer className={`footer ${navOpen ? "nav-active" : ""}`}>Footer</footer>
+    <footer className={`footer ${navOpen ? "nav-active" : ""}`}>
+      <SMLinks />
+      <hr className="hr--1" />
+      <div className="footer__links">
+        <a href="javascript:void(0)">Terms & Conditions</a>&nbsp;|&nbsp;
+        <a href="javascript:void(0)">Privacy</a>&nbsp;|&nbsp;
+        <a href="javascript:void(0)">Policies</a>
+      </div>
+      <span className="footer__copyright">
+        &copy;2020, invest (RS). All Rights Reserved.
+      </span>
+    </footer>
   );
 };
 
